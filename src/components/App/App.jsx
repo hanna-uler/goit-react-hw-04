5c5febb2bc2bfdefdc1b8e9a29fa22cd60902397
hanna-uler <hannauler@gmail.com>
2025-05-10 19:36:40 -0400

    change prettierrc, add search form markup and func

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,15 @@
 import './App.module.css'
+import SearchBar from '../SearchBar/SearchBar'
 
 export default function App() {
+  const searchPictures = (searchQuery) => {
+    console.log(searchQuery);
+  }
 
   return (
     <> 
-    <p>Project is in progress...</p>  
+      <p>Project is in progress...</p>  
+      <SearchBar onSubmit={searchPictures} />
     </>
   )
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,29 @@
 import css from "./SearchBar.module.css"
 
-export default function SearchBar() {
+export default function SearchBar({ onSubmit }) {
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        const form = evt.target;
+        const searchQuery = form.elements.searchInput.value;
+        console.log(searchQuery);
+
+        onSubmit(searchQuery);
+        form.reset()
+    }
+    return (
+        <header className={css.conteiner}>
+            <form className={css.form} onSubmit={handleSubmit}>
+                <input
+                    className={css.input}
+                    type="text"
+                    name="searchInput"
+                    autoComplete="off"
+                    autoFocus
+                    placeholder="Search images and photos"
+                />
+                <button className={css.btn} type="submit">Search</button>
+            </form>
+        </header>
+    )
     
 }
