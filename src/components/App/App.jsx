faa3b76269102467768d92af003fd6e1dd77752e
hanna-uler <hannauler@gmail.com>
2025-05-21 19:33:13 -0400

    add LoadMoreBtn, query and page states, pagination, refactor http request func

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,26 +6,52 @@ import { useState, useEffect } from 'react';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import fetchPictures from '../../pictures-api';
 import Loader from '../Loader/Loader';
+import LoadMoreBtn from '../LoadMoreBtn/LoadMoreBtn';
 
 
 export default function App() {
   const [pictures, setPictures] = useState([]);
   const [isLoading, setLoading] = useState(false);
   const [isError, setError] = useState(false);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
 
   const searchPictures = async (searchQuery) => {
-    try {
-      setError(false);
-      setPictures([]);
-      setLoading(true);
-      const data = await fetchPictures(searchQuery);
-      setPictures(data.results);      
-    } catch {
-      setError(true);
-    } finally {
-      setLoading(false);
+    setPage(1);
+    setQuery(searchQuery);
+  }
+  
+  const incrementPage = () => {
+    setPage(page + 1);
+    // console.log(page);
+  }
+
+  useEffect(() => {
+    if (query === "") {
+      return;
     }
-    }
+    fetchPictures(query, page).then((data)=>{
+      const newPicArray = data.results;
+      setPictures((prevPicArray) => {
+        return [...prevPicArray, ...newPicArray];
+      })
+    });
+    // const upgradeGallery = async (query, page) =>{
+    // try {
+    //   setError(false);
+    //   setPictures([]);
+    //   setLoading(true);
+    //   const data = await fetchPictures(query);
+    //   setPictures(data.results);
+    // } catch {
+    //   setError(true);
+    // } finally {
+    //   setLoading(false);
+    // }
+    // }
+    // upgradeGallery();
+    console.log(query, page);
+}, [query, page])
 
   return (
     <> 
@@ -34,6 +60,7 @@ export default function App() {
       {isLoading && <Loader />}
       {isError && <ErrorMessage/>}
       {pictures.length > 0 && <ImageGallery picsArray={pictures} />}
+      <LoadMoreBtn turnPage={incrementPage} />
       <Toaster position="top-right" reverseOrder={false} duration="3000" />
     </>
   )
