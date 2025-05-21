0a43a158f67e6a27703c4451de6c0e21fb996993
hanna-uler <hannauler@gmail.com>
2025-05-21 16:42:24 -0400

    add loading state and condit rendering

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,14 +5,18 @@ import ImageGallery from '../ImageGallery/ImageGallery';
 import { useState, useEffect } from 'react';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import fetchPictures from '../../pictures-api';
+import Loader from '../Loader/Loader';
 
 
 export default function App() {
   const [pictures, setPictures] = useState([]);
+  const [isLoading, setLoading] = useState(false);
 
   const searchPictures = async (searchQuery) => {
     try {
+      setLoading(true);
       const data = await fetchPictures(searchQuery);
+      setLoading(false);
       setPictures(data.results);      
     } catch (error) {
       
@@ -23,8 +27,9 @@ export default function App() {
     <> 
       <p>Project is in progress...</p>  
       <SearchBar onSubmit={searchPictures}/>
+      {isLoading && <Loader/>};
       {pictures.length > 0 && <ImageGallery picsArray={pictures} />}
-      <Toaster position="top-right" reverseOrder={false} duration="3000"/>
+      <Toaster position="top-right" reverseOrder={false} duration="3000" />
     </>
   )
 }
