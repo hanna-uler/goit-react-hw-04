3a5de2c3aa12e5bd58e416ec87fa7827fd312319
hanna-uler <hannauler@gmail.com>
2025-05-22 12:56:27 -0400

    i react-spinners, add spinner and css to Loader comp, edit index.css

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,11 @@
 import css from "./Loader.module.css"
+import { SyncLoader } from "react-spinners"
 
 export default function Loader() {
     return (
-        <>
-            <p>Please wait while the little elves pick the appropriate pictures</p>
-        </>
+        <div className={css.container}>
+            <SyncLoader color="#82c798" size={24}/>
+            <p className={css.message}>Please wait while the little elves pick the appropriate pictures</p>
+        </div>
     )
 }
